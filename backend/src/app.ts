7f910002c31227f77dda2ea4f401fbcce1ba16eb
an-sbl <an.sbl@yandex.ru>
2026-03-10 23:14:20 +0300

    fix: Проверка непустого CORS

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,12 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
+app.use(cors({ 
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(serveStatic(path.join(__dirname, 'public')))
