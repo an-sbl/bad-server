36a7e208164afdcd424a75b9b9fac87ca9aba477
an-sbl <an.sbl@yandex.ru>
2026-03-11 00:06:58 +0300

    fix: Добавлена мидлвара роли админа для получения всех заказов

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -15,7 +15,7 @@ import { Role } from '../models/user'
 const orderRouter = Router()
 
 orderRouter.post('/', auth, validateOrderBody, createOrder)
-orderRouter.get('/all', auth, validateOrdersQuery, getOrders)
+orderRouter.get('/all', auth, roleGuardMiddleware(Role.Admin), validateOrdersQuery, getOrders)
 orderRouter.get('/all/me', auth, validateUserOrdersQuery, getOrdersCurrentUser)
 orderRouter.get(
     '/:orderNumber',
