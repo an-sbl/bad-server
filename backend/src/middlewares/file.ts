eed427918690b16a13b3feef81e31253c99f4be5
an-sbl <an.sbl@yandex.ru>
2026-03-11 00:36:16 +0300

    feat: Уникальные нименования загружаемых файлов

--- backend/src/middlewares/file.ts
+++ backend/src/middlewares/file.ts
@@ -2,6 +2,7 @@ import { Request, Express } from 'express'
 import multer, { FileFilterCallback } from 'multer'
 import { mkdirSync } from 'fs'
 import { join } from 'path'
+import path from 'path'
 
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
@@ -29,7 +30,10 @@ const storage = multer.diskStorage({
         file: Express.Multer.File,
         cb: FileNameCallback
     ) => {
-        cb(null, file.originalname)
+        const uniqueName = Date.now() + '_' + Math.round(Math.random() * 1E9);
+        const ext = path.extname(file.originalname);
+        const filename = `${uniqueName}${ext}`;
+        cb(null, filename)
     },
 })
 
