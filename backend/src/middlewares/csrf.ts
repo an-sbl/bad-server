84ba7f2d479818e22e6fd306e316d9f59e3dd0a9
an-sbl <an.sbl@yandex.ru>
2026-03-11 15:14:22 +0300

    fix: Исправление куки csrf v4

--- backend/src/middlewares/csrf.ts
+++ backend/src/middlewares/csrf.ts
@@ -6,15 +6,9 @@ const {
   generateCsrfToken,
   invalidCsrfTokenError 
 } = doubleCsrf({
-  getSecret: () => 'secret-key-for-csrf',
-  getSessionIdentifier: (req) => req.cookies?.['_csrf_secret'] || '',
-  cookieName: "_csrf_secret",
-  cookieOptions: {
-    httpOnly: true,
-    sameSite: 'strict',
-    path: '/',
-    secure: false,
-  },
+  getSecret: (req) => req?.cookies?.['_csrf'] || process.env.CSRF_SECRET || 'secret-key-for-csrf',
+  getSessionIdentifier: (req) => 'session',
+  cookieName: undefined,
   getCsrfTokenFromRequest: (req) => {
     return (req.headers['x-csrf-token'] || req.headers['csrf-token']) as string;
   },
@@ -26,8 +20,8 @@ export { doubleCsrfProtection as csrfProtection };
 export const sendCsrfToken = (req: Request, res: Response, next: NextFunction) => {
   
   const token = generateCsrfToken(req, res);
-  
-  res.cookie('_csrf', 'secret-placeholder', {
+  const secret = process.env.CSRF_SECRET
+  res.cookie('_csrf', secret, {
     httpOnly: true,
     sameSite: 'strict',
     path: '/',
